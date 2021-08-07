959f4aacb9f5d9e61a2114d8a507221276ef95b6
stephenchoung <stephen.choung1@gmail.com>
2021-08-07 00:40:43 -0400

    added Gifs

--- react-app/src/components/Auth_Components/LogoutButton.js
+++ react-app/src/components/Auth_Components/LogoutButton.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/session";
 
 const LogoutButton = () => {
+  const [logoutName,setLogoutName] = useState("Logout")
   const dispatch = useDispatch();
   const onLogout = async (e) => {
+    setLogoutName("Cya Later")
+    setTimeout(() =>{
+      setLogoutName("Logout")
+    },1000)
+
     await dispatch(logout());
   };
 
-  return <button className="button" onClick={onLogout}>Logout</button>;
+  return <button className="button" onClick={onLogout}>{logoutName}</button>;
 };
 
 export default LogoutButton;
